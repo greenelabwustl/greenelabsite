cb6c338faf9fc2fa25361fdc4b2eb5a8ad4575ea
Abigail R Baim <abaim@greenelab.ucsd.edu>
2025-05-13 19:33:09 +0000

    Update settings.py

--- labsite/settings.py
+++ labsite/settings.py
@@ -32,7 +32,7 @@ else: # Do these things when DEBUG is disabled
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SECURE_SSL_REDIRECT = True
 
-ALLOWED_HOSTS = ['greenelab.wustl.edu','localhost']
+ALLOWED_HOSTS = ['greenelab.ucsd.edu','localhost']
 
 # Application definition
 
